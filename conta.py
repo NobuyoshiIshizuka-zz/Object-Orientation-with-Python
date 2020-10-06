411e3bc3ae33b612eb80f04a5c0c8f4806ef0596
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2020-10-05 22:58:47 -0300

    metodos getter and setter

--- conta.py
+++ conta.py
@@ -10,7 +10,27 @@ class Conta:
         print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.__saldo -= valor
+        self.__saldo += valor
 
     def saca(self, valor):
         self.__saldo -= valor
+
+    def trasferir(self, valor, destino):
+       self.saca(valor)
+       destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
